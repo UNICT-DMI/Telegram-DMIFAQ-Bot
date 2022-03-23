3e55c133876dd07861ad3839255c5965cd1d50cf
aegroto <aegroto@protonmail.com>
2022-03-23 09:58:36 +0100

    linting test

--- modules/commands/start.py
+++ modules/commands/start.py
@@ -3,6 +3,4 @@ from telegram.ext import CallbackContext
 
 
 def start_cmd_handler(update: Update, context: CallbackContext):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id,text="I'm a bot, please talk to me!")
